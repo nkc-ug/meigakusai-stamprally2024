1eddc2d1d2d1db676dc5b64309f759cc8a29848f
Mt.Book⚜ <mountbook.com@gmail.com>
2024-08-21 12:39:26 +0900

    初回時とそれ以降で返ってくるデータが変わってしまう不具合の修正

--- src/component/StampData.ts
+++ src/component/StampData.ts
@@ -26,19 +26,17 @@ export const GetStampData = (id: string) => {
 };
 export const DefaultStampData = () => {
   const defaultJson = JSON.stringify(
-    {
-      stampdata: [
-        { id: "152", isGet: false },
-        { id: "162", isGet: false },
-        { id: "334", isGet: false },
-        { id: "352", isGet: false },
-        { id: "618", isGet: false },
-        { id: "1032", isGet: false },
-        { id: "1042", isGet: false },
-        { id: "01", isGet: false },
-        { id: "02", isGet: false },
-      ],
-    },
+    [
+      { id: "152", isGet: false },
+      { id: "162", isGet: false },
+      { id: "334", isGet: false },
+      { id: "352", isGet: false },
+      { id: "618", isGet: false },
+      { id: "1032", isGet: false },
+      { id: "1042", isGet: false },
+      { id: "01", isGet: false },
+      { id: "02", isGet: false },
+    ],
     null,
     2
   );
