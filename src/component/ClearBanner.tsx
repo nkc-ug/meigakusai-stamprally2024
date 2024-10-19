a59ae35eb0c1b54f17e0f3c7b8141d082666811d
Mount-Book <mountbook.com@gmail.com>
2024-10-19 09:23:22 +0900

    模擬店画像の追加 / 催し物カテゴリの追加

--- src/component/ClearBanner.tsx
+++ src/component/ClearBanner.tsx
@@ -3,12 +3,11 @@ import { Button, Typography } from "@mui/material";
 export const ClearBanner = () => {
   return (
     <Button
-      sx={{
-        backgroundColor: "blue",
-      }}
+      variant="outlined"
       onClick={() => {
         console.log("交換画面遷移");
       }}
+      sx={{ border: "solid 5px", borderRadius: "10px" }}
     >
       <Typography variant="h4">景品交換画面はこちらから</Typography>
     </Button>
--- src/component/StampImage.ts
+++ src/component/StampImage.ts
@@ -26,6 +26,40 @@ import stamp24 from "/src/assets/images/stamp/24.jpg";
 import stamp25 from "/src/assets/images/stamp/25.jpg";
 import stamp26 from "/src/assets/images/stamp/26.jpg";
 import stamp27 from "/src/assets/images/stamp/27.jpg";
+import stamp120 from "/src/assets/images/stamp/120.jpg";
+import stamp135 from "/src/assets/images/stamp/135.jpg";
+import stamp144 from "/src/assets/images/stamp/144.jpg";
+import stamp145 from "/src/assets/images/stamp/145.jpg";
+import stamp155 from "/src/assets/images/stamp/155.jpg";
+import stamp163 from "/src/assets/images/stamp/163.jpg";
+import stamp165 from "/src/assets/images/stamp/165.jpg";
+import stamp174 from "/src/assets/images/stamp/174.jpg";
+import stamp336 from "/src/assets/images/stamp/336.jpg";
+import stamp346 from "/src/assets/images/stamp/346.jpg";
+import stamp347 from "/src/assets/images/stamp/347.jpg";
+import stamp351 from "/src/assets/images/stamp/351.jpg";
+import stamp353 from "/src/assets/images/stamp/353.jpg";
+import stamp354 from "/src/assets/images/stamp/354.jpg";
+import stamp357 from "/src/assets/images/stamp/357.jpg";
+import stamp618 from "/src/assets/images/stamp/618.jpg";
+import stamp1041 from "/src/assets/images/stamp/1041.jpg";
+import stamp1043 from "/src/assets/images/stamp/1043.jpg";
+import stamp1046 from "/src/assets/images/stamp/1046.jpg";
+import stamp1051 from "/src/assets/images/stamp/1051.jpg";
+import stampE01 from "/src/assets/images/stamp/E01.jpg";
+import stampE02 from "/src/assets/images/stamp/E02.jpg";
+import stampE03 from "/src/assets/images/stamp/E03.jpg";
+import stampE04 from "/src/assets/images/stamp/E04.jpg";
+import stampE05 from "/src/assets/images/stamp/E05.jpg";
+import stampE06 from "/src/assets/images/stamp/E06.jpg";
+import stampE07 from "/src/assets/images/stamp/E07.jpg";
+import stampE08 from "/src/assets/images/stamp/E08.jpg";
+import stampE09 from "/src/assets/images/stamp/E09.jpg";
+import stampE10 from "/src/assets/images/stamp/E10.jpg";
+import stampE11 from "/src/assets/images/stamp/E11.jpg";
+import stampE12 from "/src/assets/images/stamp/E12.jpg";
+import stampE13 from "/src/assets/images/stamp/E13.jpg";
+import stampE14 from "/src/assets/images/stamp/E14.jpg";
 
 export const StampImage = (stamp: string) => {
   const STAMPIMAGE_LIST: { [key: string]: string } = {
@@ -57,6 +91,40 @@ export const StampImage = (stamp: string) => {
     "25": stamp25,
     "26": stamp26,
     "27": stamp27,
+    "120": stamp120,
+    "135": stamp135,
+    "144": stamp144,
+    "145": stamp145,
+    "155": stamp155,
+    "163": stamp163,
+    "165": stamp165,
+    "174": stamp174,
+    "336": stamp336,
+    "346": stamp346,
+    "347": stamp347,
+    "351": stamp351,
+    "353": stamp353,
+    "354": stamp354,
+    "357": stamp357,
+    "618": stamp618,
+    "1041": stamp1041,
+    "1043": stamp1043,
+    "1046": stamp1046,
+    "1051": stamp1051,
+    E01: stampE01,
+    E02: stampE02,
+    E03: stampE03,
+    E04: stampE04,
+    E05: stampE05,
+    E06: stampE06,
+    E07: stampE07,
+    E08: stampE08,
+    E09: stampE09,
+    E10: stampE10,
+    E11: stampE11,
+    E12: stampE12,
+    E13: stampE13,
+    E14: stampE14,
   };
 
   return STAMPIMAGE_LIST[stamp] ?? sample;
